ac8713e993222057ea556ff4aa3f500c67842b7a
Helios Figueroa <helioss.figueroa@gmail.com>
2020-08-10 21:59:21 -0500

    Event sync
    Add event sync in case some service down

--- comments-service/index.js
+++ comments-service/index.js
@@ -39,8 +39,28 @@ app.post('/posts/:id/comments', async (req, res) => {
 	res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
 	console.log('Received Event:', req.body.type);
+
+	const { type, data } = req.body;
+
+	if (type === 'CommentModerated') {
+		const { id, status, content, postId } = data;
+		const comments = commentsByPostId[postId];
+		const comment = comments.find(comment => comment.id === id);
+		comment.status = status;
+
+		await axios.post('http://localhost:4005/events', {
+			type: 'CommentUpdated',
+			data: {
+				id,
+				status,
+				postId,
+				content
+			}
+		});
+	}
+
 	res.send({});
 });
 
--- event-bus/index.js
+++ event-bus/index.js
@@ -5,14 +5,23 @@ const axios = require('axios');
 const app = expres();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', async (req, res) => {
   const event = req.body;
+  events.push(event);
 
   axios.post('http://localhost:4000/events', event);
   axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/posts', event);
+  axios.post('http://localhost:4002/events', event);
+  axios.post('http://localhost:4003/events', event);
 
   res.send({ status: 'ok' })
+});
+
+app.get('/events', (req, res) => {
+  console.log('serving events:', events)
+  res.send(events);
 });
 
 app.listen(4005, () => {
